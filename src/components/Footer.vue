c0d31a599b1684182448d7464e226ffd079b7c92
RB <RBercasio22@gmail.com>
2024-07-10 18:59:41 -0700

    First Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-footer
+      class="bg-transparent text-center d-flex flex-column"
+    >
+      <div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          :icon="icon"
+          class="mx-4"
+          variant="text"
+        ></v-btn>
+      </div>
+  
+      <div class="pt-0">
+        <p style="font-size: 11px; letter-spacing: 1px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore quaerat atque tempore. Harum quo temporibus enim voluptatibus sequi cupiditate. Dolorem, autem reiciendis. Sequi exercitationem consequatur enim repudiandae! Eius, ex quia quos ipsam atque qui architecto. Cumque, nostrum facilis veritatis nam rem a est omnis accusantium inventore eum. Inventore labore obcaecati, dolorem omnis quasi ratione sit possimus. Corporis veritatis quo, quibusdam molestias facere similique sed amet? Minima, necessitatibus aspernatur. Nisi cum ut debitis itaque ex! Tenetur deserunt voluptas, obcaecati iste pariatur blanditiis? Tempora nemo dolores praesentium aperiam consectetur, temporibus omnis nisi facilis magni unde inventore earum qui blanditiis commodi aliquid harum?</p>
+       
+      </div>
+  
+      <v-divider></v-divider>
+  
+      <div>
+        {{ new Date().getFullYear() }} — <strong>Developed by RB</strong>
+      </div>
+    </v-footer>
+  </template>
+  <script>
+    export default {
+      data: () => ({
+        icons: [
+          'mdi-facebook',
+          'mdi-twitter',
+          'mdi-linkedin',
+          'mdi-instagram',
+        ],
+      }),
+    }
+  </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,90 @@
+<template>
+        <div class="center-nav">
+        
+             <h1>
+                <v-icon icon="mdi-electron-framework"></v-icon>
+                App
+            </h1>
+
+            <div class="menu-pos">
+                <h2><v-icon icon="mdi-menu"></v-icon></h2>
+            </div>
+          
+        </div>
+       
+        <div class="sidenav">
+            <p class="p-title">
+                <v-icon icon="mdi-fire"></v-icon>
+                  START WITH FRAMEWORK</p>
+            <ul>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                
+             
+            </ul>
+            <v-divider></v-divider>
+             <!--Make dynamic using loops(v-for)-->
+            <p class="p-title">FRAMEWORK QUICKSTARTS</p>
+            <ul>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+             
+            </ul>
+              <!--Make dynamic using loops(v-for)-->
+            <v-divider></v-divider>
+            <p class="p-title">WEB APP DEMOS</p>
+            <ul>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+               
+            </ul>
+            <v-divider></v-divider>
+            <!--Make dynamic using loops(v-for)-->
+            <p class="p-title">MOBILE TUTORIALS</p>
+            <ul>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+                <li>Dummy text</li>
+             
+            </ul>
+            <v-divider></v-divider>
+        
+        </div>
+   
+    <div class="main">
+        <div class="todo-content">
+            <Todo />
+        </div>
+       
+        <div class="footer-content">
+           <Footer />
+        </div>
+       
+    </div>
+   
+</template>
+
+<script>
+//<Todo />
+</script>
